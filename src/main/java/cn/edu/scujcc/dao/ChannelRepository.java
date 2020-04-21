9b3330ccdad8e4860c7dc5598918097a0e2042ca
Eric <58718764+E-J122@users.noreply.github.com>
2020-04-21 17:03:33 +0800

    已完成后台API的基本开发，频道的增删查改功能已完成。

---
+++ src/main/java/cn/edu/scujcc/dao/ChannelRepository.java
@@ -0,0 +1,17 @@
+package cn.edu.scujcc.dao;
+
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import cn.edu.scujcc.model.Channel;
+@Repository
+public interface ChannelRepository extends MongoRepository<Channel,String> {
+	public List<Channel> findByTitle(String t);
+	public List<Channel> findByQuality(String q);
+	//找出评论时间在指定日期之后的所以频道
+	public List<Channel> findByCommentsDtAfter(LocalDateTime theDt);
+}
